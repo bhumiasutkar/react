1973478bb41b031be57aa94ba7a67750d58b2b38
Bhuvneshwari <bhumisasutkar@gmail.com>
2020-09-05 16:38:39 +0530

    commit movie app

---
+++ movie-list/src/component/movie-item.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Movie extends Component {
+  state = {};
+  render() {
+    const item = this.props;
+    return (
+      <tr>
+        <td>{item.title}</td>
+        <td>{item.genre}</td>
+        <td>{item.stock}</td>
+        <td>{item.rate}</td>
+        <td>
+          <button
+            className="btn btn-danger"
+            value={item.action}
+            onClick={() => item.onDelete(item.id)}
+          >
+            Remove
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default Movie;
