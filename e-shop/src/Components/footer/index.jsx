2e59c128d8bb66d8eb9d1979571973c090225af1
Bhuvneshwari <bhumisasutkar@gmail.com>
2020-09-26 22:51:47 +0530

    commit

---
+++ e-shop/src/Components/footer/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+//import "footer.scss";
+
+const Footer = () => {
+  return <h1>footer</h1>;
+};
+export default Footer;
---
+++ e-shop/src/Components/homeslider/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class HomeSlider extends Component {
+  render() {
+    return <h1> Home Slider</h1>;
+  }
+}
+export default HomeSlider;
