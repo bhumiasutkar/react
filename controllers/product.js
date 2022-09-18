1f95cceb4bcf93f13438ce05e69cff93c98fbb3b
Bhuvneshwari <bhumisasutkar@gmail.com>
2022-09-18 19:14:17 +0530

    commit

--- controllers/product.js
+++ controllers/product.js
@@ -3,6 +3,7 @@ const Product = require('../models/product');
 
 exports.create = (req, res) => {
     const product = new Product(req.body);
+
     product.save((err, data) => {
         if (err) {
             res.status(400).json({
